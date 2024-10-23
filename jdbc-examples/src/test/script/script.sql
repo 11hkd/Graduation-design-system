ddec98e50ad141d7fd698ec5d9f462fc83396b48
hdzAa <150504424+11hkd@users.noreply.github.com>
2024-10-23 20:15:51 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	README.md

--- jdbc-examples/src/test/script/script.sql
+++ jdbc-examples/src/test/script/script.sql
@@ -1,5 +1,5 @@
 
-select * from address a where a.user_id='1283960607856160768'
+select * from address a where a.user_id='1283960607856160768';
 
 
 select * from address a,user u
@@ -7,16 +7,10 @@ where a.user_id=u.id and a.id='2222';
 
 
 select * from user u join address a
-on u.id=a.user_id
+on u.id=a.user_id;
 
 select u.id as user_id,u.name,count(a.user_id) as count from user u join address a
 on u.id=a.user_id
 group by u.id
 order by count desc;
 
-select u.id as user_id,u.name,count(a.user_id) as count from user u join address a
-                                                                         on u.id=a.user_id
-group by u.id
-order by count desc;
-select u.id as user_id,u.name,count(a.user_id) as count from user u join address a
-                                                                         on u.id=a.user_id
