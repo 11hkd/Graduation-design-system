ff199ecdba201d8f73132a4616c19bde486e4254
hdzAa <150504424+11hkd@users.noreply.github.com>
2024-10-24 21:40:07 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	README.md

--- jdbc-examples/src/test/script/script.sql
+++ jdbc-examples/src/test/script/script.sql
@@ -7,9 +7,3 @@ where a.user_id=u.id and a.id='2222';
 
 select * from user u join address a
 on u.id=a.user_id;
-
-select u.id as user_id,u.name,count(a.user_id) as count from user u join address a
-on u.id=a.user_id
-group by u.id
-order by count desc;
-
